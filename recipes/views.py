283075f4bf1f9ca81e430b4e93a1eecb06159b29
SamuelHosken <samuelhosken.o@gmail.com>
2024-07-17 18:37:21 -0300

    Tirando contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -4,8 +4,3 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/home.html', {'name': 'Hosken'})
 
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
